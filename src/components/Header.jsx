75855b207dc144bef5ec493709956828c74f8e2e
Saifihassan <saifihassan656@gmail.com>
2025-10-24 13:50:00 +0530

    ~

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,51 @@
 import React, { useContext } from "react";
-import sunicon from "../assets/sun.svg"
-import moonicon from "../assets/moon.svg"
-import hamburger from "../assets/hamburger.svg"
+import sunicon from "../assets/sun.svg";
+import moonicon from "../assets/moon.svg";
+import hamburger from "../assets/hamburger.svg";
 import { NoteContext } from "../App";
 function Header() {
-  const [state,dispatch,Theme,setTheme,lightTheme,darkTheme,note] = useContext(NoteContext)
+  const [state, dispatch, Theme, setTheme, lightTheme, darkTheme, note] =
+    useContext(NoteContext);
   return (
-    <header className={`z-20 ${Theme==="light"?lightTheme.lightTheme:darkTheme.darkTheme}`}>
+    <header
+      className={`z-20 ${
+        Theme === "light" ? lightTheme.lightTheme : darkTheme.darkTheme
+      }`}
+    >
       {" "}
-      <button className={`transition-all duration-150 ${Theme === "light" ? "hover:drop-shadow-lg" : "hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)]"}`}><img src={hamburger} alt="" /></button>
-     <p className={`heading-animation transition-all duration-150 ${Theme === "light" ? "hover:drop-shadow-lg" : "hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)]"}`}>The Keeper</p>
-
-      <button className={`transition-all duration-300 ${Theme === "light" ? "hover:drop-shadow-lg" : "hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)]"}`} onClick={()=>{Theme==="light"?setTheme("dark"):setTheme("light")}}>{Theme==="light"?(<img src={moonicon}/>):(<img src={sunicon}/>)}</button>
-     
+      <button
+        className={`transition-all duration-150 ${
+          Theme === "light"
+            ? "hover:drop-shadow-lg"
+            : "hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)]"
+        }`}
+        onClick={()=>{
+          alert("This feature is under development and will be made available shortly")
+        }}
+      >
+        <img src={hamburger} alt="" />
+      </button>
+      <p
+        className={`heading-animation transition-all duration-150 ${
+          Theme === "light"
+            ? "hover:drop-shadow-lg"
+            : "hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)]"
+        }`}
+      >
+        The Keeper
+      </p>
+      <button
+        className={`transition-all duration-300 ${
+          Theme === "light"
+            ? "hover:drop-shadow-lg"
+            : "hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)]"
+        }`}
+        onClick={() => {
+          Theme === "light" ? setTheme("dark") : setTheme("light");
+        }}
+      >
+        {Theme === "light" ? <img src={moonicon} /> : <img src={sunicon} />}
+      </button>
     </header>
   );
 }
